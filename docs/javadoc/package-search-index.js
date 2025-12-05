e93f79c3859b7d0e5679625bf9151f2c56761c63
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2025-12-05 11:50:36 -0500

    Deprecate this repo.

--- docs/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.silabs.na.pcap"},{"l":"com.silabs.na.pcap.impl"},{"l":"com.silabs.na.pcap.util"}]
--- pcap/src/main/java/module-info.java
+++
@@ -1,4 +0,0 @@
-module com.silabs.na.pcap {
-  exports com.silabs.na.pcap;
-  exports com.silabs.na.pcap.util;
-}
